155bd700cc48d271c5a310496c4be3849925fafe
andrelucena <andrelucenajr123@gmail.com>
2024-04-06 20:39:54 -0300

    Criaçao do model UserPerfil

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class Item_pedido_inline(admin.TabularInline):
+    model = models.ItemPedido
+    extra = 1
+
+
+class Pedido_admin(admin.ModelAdmin):
+    inlines = [
+        Item_pedido_inline
+    ]
+
+
+admin.site.register(models.Pedido, Pedido_admin)
+admin.site.register(models.ItemPedido)
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserPerfil(models.Model):
+    age = models.PositiveIntegerField(150)
+    birthday = models.DateTimeField()
+    cpf = models.CharField(max_length=11)
+    address = models.CharField(max_length=400)
+    addres_number = models.CharField(max_length=1)
+    complement = models.CharField(max_length=1)
+    bairro = models.CharField(max_length=1)
+    cep = models.CharField(max_length=25)
+    city = models.CharField(max_length=1)
+    state = models.CharField(
+        max_length=2,
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
